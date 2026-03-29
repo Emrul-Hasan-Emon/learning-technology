42eb99f6a87863ad469751ef344bb26e75417538
emrul-emon <emrul.hasan@ghl.hasan>
2026-03-29 15:43:23 +0600

    add initial Go files for hello world and values demonstration

---
+++ go/values.go
@@ -0,0 +1,14 @@
+// run the program with `go run values.go`
+
+package main
+
+import "fmt"
+
+func valuesDemo() {
+	fmt.Println("go" + "lang")
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
