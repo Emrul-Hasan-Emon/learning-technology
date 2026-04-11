39e169d2cdc8d5cac26d337756ba5060ca1d0fa0
Emrul Hasan Emon <emrulhasanemon12@gmail.com>
2026-04-11 16:25:20 +0600

    js: added examples demonstrating basic statements and semicolon usage in JavaScript

---
+++ js/statement.js
@@ -0,0 +1,28 @@
+// --------------------------------------------------------------------------------
+// STATEMENTS
+// --------------------------------------------------------------------------------
+/**
+ * A computer program is a list of "instructions" to be "executed" by a computer.
+ * These programming instructions are called statements.
+ * Most JavaScript programs contain many statements.
+ * The statements are executed, one by one, in the same order as they are written.
+ */
+let x, y, z;    // Statement 1
+x = 5;          // Statement 2
+y = 6;          // Statement 3
+z = x + y;      // Statement 4
+
+// --------------------------------------------------------------------------------
+// SEMICOLONS
+// --------------------------------------------------------------------------------
+/**
+ * Semicolons separate JavaScript statements.
+ * Add a semicolon at the end of each executable statement:
+ */
+ let a, b, c;  // Declare 3 variables
+a = 5;        // Assign the value 5 to a
+b = 6;        // Assign the value 6 to b
+c = a + b;    // Assign the sum of a and b to c
+
+// When separated by semicolons, multiple statements on one line are allowed
+a = 5; b = 6; c = a + b;
