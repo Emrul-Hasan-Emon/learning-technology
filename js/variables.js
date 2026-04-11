f778d86615f8b6059c50538f6243ad8aa90da39d
Emrul Hasan Emon <emrulhasanemon12@gmail.com>
2026-04-11 16:25:29 +0600

    js: added comprehensive examples demonstrating variable declarations and usage in JavaScript

---
+++ js/variables.js
@@ -0,0 +1,45 @@
+// --------------------------------------------------------------------------------
+// VARIABLES
+// --------------------------------------------------------------------------------
+/**
+ * Variables = Data Containers
+ * JavaScript variables are containers for data.
+ * JavaScript variables can be declared in 4 ways:
+ * 1. Using let
+ * 2. Using var (Not Recommended)
+ * 3. Using const
+ * 4. Automatically (Not Recommended)
+ */
+
+// Examples Using let
+let x = 5;
+let y = 6;
+let z = x + y;
+
+// Example Using const
+const x = 5;
+const y = 6;
+const z = x + y;
+
+// Example Using _ (underscore)
+let _lastName = "Johnson";
+let _x = 2;
+let _100 = 5;
+
+// Example Using $ (dollar sign)
+let $ = "Hello World";
+let $$$ = 2;
+let $myMoney = 5;
+
+// Example Using var (Not Recommended)
+var x = 5;
+var y = 6;
+var z = x + y;
+
+// Example - Declaring Variables Automatically ((Not Recommended))
+x = 5;
+y = 6;
+z = x + y;
+
+// Example - One Statement, Declaring Multiple Variables
+let person = "John Doe", carName = "Volvo", price = 200;
