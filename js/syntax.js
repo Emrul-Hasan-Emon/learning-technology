4a967f8bc691ab29b11ed4e3c61999c33e1fdb97
Emrul Hasan Emon <emrulhasanemon12@gmail.com>
2026-04-11 16:24:59 +0600

    js: added implementation of JavaScript syntax examples with literals and variables

---
+++ js/syntax.js
@@ -0,0 +1,20 @@
+/**
+ * The JavaScript syntax defines two types of values
+ * Literals - Fixed values
+ * Variables - Variable values
+ * 
+ * JavaScript keywords are used to defines actions to be performed.
+ * JavaScript keywords are case-sensitive.
+ * JavaScript does not interpret LET or Let as the keyword let.
+ */
+
+// Fixed values
+let x = 5;
+let y = 6;
+
+// Variable Values. Because z value depends on x and y
+let z = x + y;
+
+console.log('x = ', x);
+console.log('y = ', y);
+console.log('z = ', z)
