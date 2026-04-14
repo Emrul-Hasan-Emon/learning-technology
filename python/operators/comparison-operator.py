38abced455ba54931a8ff02bca83076e3d4f1e8a
Emrul Hasan Emon <emrulhasanemon12@gmail.com>
2026-04-14 13:46:34 +0600

    python: added examples for comparison operators

---
+++ python/operators/comparison-operator.py
@@ -0,0 +1,21 @@
+# ==	    Equal	                    x == y	
+# !=	    Not equal	                x != y	
+# >	        Greater than	            x > y	
+# <	        Less than	                x < y	
+# >=	    Greater than or equal to	x >= y	
+# <=	    Less than or equal to	    x <= y
+
+x = 5
+y = 3
+
+print(x == y) # Output - False
+print(x > y) # Output - True
+print(x < y) # Output - False
+print(x >= y) # Output - True
+print(x <= y) # Output - False
+
+# Chaining Comparison Operators
+x = 5
+
+print(1 < x < 10) # Output - True
+print(1 < x and x < 10) # Output - True
