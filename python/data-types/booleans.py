f82056d828913b5b58540712f99a4dd5c90eb947
Emrul Hasan Emon <emrulhasanemon12@gmail.com>
2026-04-14 13:28:12 +0600

    python: added examples for boolean type

---
+++ python/data-types/booleans.py
@@ -0,0 +1,29 @@
+# Boolean Values can only be True or False.
+print(10 > 9) # Output - True
+
+print(10 == 9) # Output - False
+
+print(10 < 9) # Output - False
+
+a = 200
+b = 33
+print(a > b) # Output - True
+
+print(bool("Hello")) # Output - True
+
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+print(bool("Hello"))
+print(bool(15))
+
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
