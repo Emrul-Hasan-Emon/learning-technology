2b3e8dc4442851699265758d6f4e2ef46c173bdb
Emrul Hasan Emon <emrulhasanemon12@gmail.com>
2026-04-14 13:46:15 +0600

    python: added examples for arithmetic operators

---
+++ python/operators/arithmetic-operator.py
@@ -0,0 +1,33 @@
+# +	        Addition	    x + y	
+# -	        Subtraction	    x - y	
+# *	        Multiplication	x * y	
+# /	        Division	    x / y	
+# %	        Modulus	        x % y	
+# **	    Exponentiation	x ** y	
+# //	    Floor division	x // y
+
+x = 15
+y = 4
+
+print(x + y) # Output - 19
+print(x - y) # Output - 11
+print(x * y) # Output - 60
+print(x / y) # Output - 3.75
+print(x % y) # Output - 3
+print(x ** y) # Output - 50625
+print(x // y) # Output - 3
+
+# Python has two division operators:
+# The / operator will always return a float value, while the // operator will return an integer value if both operands are integers, and a float value if either operand is a float.
+x = 12
+y = 5
+
+print(x / y) # Output - 2.4
+print(x // y) # Output - 2
+
+# Floor division always returns an integer.
+# The floor division // rounds the result down to the nearest whole number.
+x = 12
+y = 5
+
+print(x // y) # Output - 2
